0232a2d3e846499b2dcc0df8770409b0a8f8acdf
Agung Saputra <agungsaputradifh@gmail.com>
2023-01-22 09:44:42 +0700

    make add function

--- Web/To Do List/main.js
+++ Web/To Do List/main.js
@@ -1,6 +1,7 @@
 // Create a "close" button and append it to each list item
 const myNodeList = document.getElementsByTagName("li");
-for (let i = 0; i < myNodeList.length; i++) {
+let i;
+for (i = 0; i < myNodeList.length; i++) {
   const span = document.createElement("span");
   const txt = document.createTextNode("\u00D7");
   span.className = "close";
@@ -28,3 +29,31 @@ list.addEventListener(
   },
   false
 );
+
+// // Create a new list item when clicking on the "Add" button
+function newElement() {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("myInput").value;
+  const text = document.createTextNode(inputValue);
+  li.appendChild(text);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  // agar title kembali kosong
+  document.getElementById("myInput").value = "";
+
+  let span = document.createElement("span");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].onclick = function () {
+      const div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
